83bb97de6b5b681a3ab160ce6abb799a4066641d
davidoevans <david.evans@modjava.com>
2016-02-11 21:04:45 -0500

    added inheritance example

---
+++ interview/inheritance.py
@@ -0,0 +1,83 @@
+class A(object):
+    def go(self):
+        print("go A go!")
+
+    def stop(self):
+        print("stop A stop!")
+
+    def pause(self):
+        raise Exception("Not Implemented")
+
+
+class B(A):
+    def go(self):
+        super(B, self).go()
+        print("go B go!")
+
+
+class C(A):
+    def go(self):
+        super(C, self).go()
+        print("go C go!")
+
+    def stop(self):
+        super(C, self).stop()
+        print("stop C stop!")
+
+
+class D(B, C):
+    def go(self):
+        super(D, self).go()
+        print("go D go!")
+
+    def stop(self):
+        super(D, self).stop()
+        print("stop D stop!")
+
+    def pause(self):
+        print("wait D wait!")
+
+
+class E(B, C): pass
+
+
+a = A()
+b = B()
+c = C()
+d = D()
+e = E()
+
+# specify output from here onwards
+
+a.go()
+print("-----")
+b.go()
+print("-----")
+c.go()
+print("-----")
+d.go()
+print("-----")
+e.go()
+print("-----")
+
+a.stop()
+print("-----")
+b.stop()
+print("-----")
+c.stop()
+print("-----")
+d.stop()
+print("-----")
+e.stop()
+print("-----")
+
+a.pause()
+print("-----")
+b.pause()
+print("-----")
+c.pause()
+print("-----")
+d.pause()
+print("-----")
+e.pause()
+print("-----")
