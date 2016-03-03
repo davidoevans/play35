eedc4e5e48ac078671785213f6fe845749e9f0b8
davidoevans <david.evans@modjava.com>
2016-03-03 06:42:05 -0500

    add initial py.tests

--- practice/test/test_string.py
+++ practice/test/test_string.py
@@ -6,7 +6,7 @@ from practice.string import make_anagram
 class Test(TestCase):
 
     def test1(self):
-        assert (True == True) 
+        assert (True == True)
 
     def test_make_anagram(self):
         assert make_anagram('aaabbb') == 'YES'
