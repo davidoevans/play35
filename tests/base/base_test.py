5c0b5124e5afc4487468fca2be547007036ae409
davidoevans <david.evans@modjava.com>
2016-03-19 14:07:24 -0400

    added some documentation to the current base_test implementation

--- tests/base/base_test.py
+++ tests/base/base_test.py
@@ -1,3 +1,33 @@
+"""
+# Overview
+This module is intended to provide a test harness that mimics the behaviour seen on the hackerrank site.  Using
+this harness enables localized testing of the hackerrank challenges and the resulting code can be posted
+verbatum to the hackerrank site.
+
+The general behaviour goes as follows:
+
+* test data is read from a input file
+* each line test data is received via STDIN using input()
+* results are written to STDOUT
+* STDOUT is captured and validated against file containing the expected output
+
+By default this, the script to be tested as well as the input and output data are assumed to be in a
+directory/package structure parallel to the test script.
+
+* test_path - tests/<path_to_test_script>
+* module_to_test - <path_to_test_script>
+* input data - data/<path_to_test_script>/<test_name>_<test_id>.in
+* output data - data/<path_to_test_script>/<test_name>_<test_id>.out
+
+# Limitations of this implementation
+1. Cannot debug the script during testing. Attempts to set breakpoints in an IDE will not be recognized and
+programatically inserting interupts using `IPython.core.debugger` will cause test failure.  This is due to the
+fact that the script to be tested is executed as a `subprocess`.
+2. Only supports the parallel directory structure described above.
+3. Sloppy and likely poor use of inheritance.
+4. Test that inherit from BaseTest consist of duplicate copy/paste implementations.
+
+"""
 import os
 import pytest
 import subprocess
@@ -19,7 +49,9 @@ def pytest_generate_tests(metafunc):
 
 
 class BaseTest:
-
+    """
+    This is meant to provide a test harness for
+    """
     def __init(self, filename=__file__):
         self.filename = filename
 
