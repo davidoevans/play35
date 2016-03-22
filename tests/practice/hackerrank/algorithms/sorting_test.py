6907150a3310df7a1259dbbcc2b7625df0b8533c
davidoevans <david.evans@modjava.com>
2016-03-22 06:23:41 -0400

    added insertion_sort as provided for the correctness_invariant challenge

--- tests/practice/hackerrank/algorithms/sorting_test.py
+++ tests/practice/hackerrank/algorithms/sorting_test.py
@@ -2,6 +2,7 @@ import pytest
 from tests.base import open_to_stdin
 from practice.hackerrank.algorithms.sorting import insertion_sort1
 from practice.hackerrank.algorithms.sorting import insertion_sort2
+from practice.hackerrank.algorithms.sorting import insertion_sort3
 
 BASE_DIR = "/Users/davidevans/PycharmProjects/play35/"
 
@@ -28,6 +29,24 @@ def test_insertion_sort2(capsys):
         n = input()
         l = list(map(int, input().split()))
         insertion_sort2(l)
+
+    out, err = capsys.readouterr()
+    expected = open(out_path, "r").read()
+    assert out == expected
+
+
+@pytest.mark.parametrize("in_data, out_data", [
+    ("tests/data/practice/hackerrank/algorithms/sorting/correctness_invariant_sample.in",
+     "tests/data/practice/hackerrank/algorithms/sorting/correctness_invariant_sample.out")
+])
+def test_correctness_invariant(capsys, in_data, out_data):
+    in_path = BASE_DIR + in_data
+    out_path = BASE_DIR + out_data
+    with open_to_stdin(in_path):
+        m = int(input().strip())
+        ar = [int(i) for i in input().strip().split()]
+        insertion_sort3(ar)
+        print(" ".join(map(str, ar)))
 
     out, err = capsys.readouterr()
     expected = open(out_path, "r").read()
